5a76b6fad8bfdf094afc745df2f601661919ea06
ldimuro <ldimuro@asu.edu>
2025-07-10 10:43:44 -0700

    added functionality to obtain tracking and play data

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,30 @@
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 
+def get_passing_plays():
+    print('getting passing plays')
+
+
+
+def get_tracking_data(year, week_start, week_end):
+    #TODO: Add dynamic way to obtain data if repo is copied from Github
+
+    tracking_data = []
+
+    for week in range(week_start, week_end+1):
+        file_path = f'/Volumes/T7/Machine_Learning/Datasets/NFL/tracking_data/{year}_tracking_week_{week}.csv'
+        tracking_data.append(pd.read_csv(file_path))
+        print(f'loaded {file_path}')
+
+    return tracking_data
+
+def get_play_data(year, pass_only=True):
+    file_path = f'/Volumes/T7/Machine_Learning/Datasets/NFL/misc/plays_{year}.csv'
+    data = pd.read_csv(file_path)
+
+    if pass_only:
+        data = data[data['passResult'].notna()]
+
+    return data
+
+    
--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print('test')
+import numpy as np
+# import get_data
+# from passing_down import PassingDown
+# import random_tree
+# import fnn
+import time
+import pandas as pd
+import os
+# from fnn import FNN
+import data_processing
+import torch
+import random
+from sklearn.utils import check_random_state
+import matplotlib.pyplot as plt
+
+
+def set_seed(seed_value=42):
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+    check_random_state(seed_value)
+
+
+def main():
+    print('running main')
+    set_seed()
+
+    # tracking_data = data_processing.get_tracking_data(year=2022, week_start=1, week_end=1)
+    # print('TOTAL WEEKS OF TRACKING DATA:', len(tracking_data))
+
+    play_data = data_processing.get_play_data(year=2022, pass_only=True)
+    print(len(play_data))
+    print(play_data.head())
+
+
+
+
+if __name__ == "__main__":
+    main()
